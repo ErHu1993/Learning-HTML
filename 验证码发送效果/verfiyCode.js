0150cf631cb19309be1a11bd65f2f6b3552c3667
huguangyu <199301055@qq.com>
2017-07-26 14:01:13 +0800

    完成验证码倒计时

--- 验证码发送效果/verfiyCode.js
+++ 验证码发送效果/verfiyCode.js
@@ -7,7 +7,24 @@ $(document).ready(function() {
         'margin-left': '500px'
     })
 
-    $('input:button').on('onclick', function() {
+    var timer = null;
+    var total = 0;
 
+    $('input:button').on('click', function() {
+        total = 3;
+        $('input:button').attr('value', total + "秒后重试");
+        $('input:button').attr('disabled', 'disabled');
+        timer = setInterval(function() {
+            total -= 1;
+            $('input:button').attr('value', total + "秒后重试");
+            if (total == 0) {
+                clearInterval(timer);
+                $('input:button').attr('value', "发送验证码");
+                $('input:button').removeAttr('disabled');
+                console.log($('input:button').attr('disabled'));
+            }
+        }, 1000);
     });
+
+
 });
