b519b22465d9f09e93ea358289ce508d88b2ea7d
胡广宇 <199301055@qq.com>
2017-09-27 09:52:17 +0800

    no message

---
+++ 自己写的Vue(Vux)文件/team-manager/expand-row.vue
@@ -0,0 +1,27 @@
+<style scoped>
+    .expand-row{
+        margin-left: 15px;
+    }
+
+</style>
+<template>
+    <div>
+        <Row class="expand-row">
+            <Col span="8" class="col">
+                <span class="expand-key">直属上级名称：</span>
+                <span class="expand-value">{{ row.name }}</span>
+            </Col>
+            <Col span="8" class="col">
+                <span class="expand-key">联系方式：</span>
+                <span class="expand-value">{{ row.contactMobile }}</span>
+            </Col>
+        </Row>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            row: Object
+        }
+    };
+</script>
