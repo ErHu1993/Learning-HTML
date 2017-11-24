d9d3a7a5dac42ba7e2d1abd1570d4aa04d28fa69
胡广宇 <199301055@qq.com>
2017-11-24 14:48:25 +0800

    no message

--- 自己写的Vue(Vux)文件/personal-center/cer-detail.vue
+++ 自己写的Vue(Vux)文件/personal-center/cer-detail.vue
@@ -26,131 +26,127 @@
 </template>
 
 <script>
+import {XButton} from 'vux'
+import cerInformation from '@/views/component-views/cer-information.vue'
 
-import { XButton} from 'vux'
-import cerInformation from '@/views/component-views/cer-information.vue';
+export default {
 
-    export default {
+  components: {
+    cerInformation,
+    XButton
+  },
 
-        components: {
-            cerInformation,
-            XButton
-        },
-
-        data () {
-            return {
-                canEdit : false,
-                imageSrcFront:'',
-                imageSrcBack:'',
-                userModel:{
-                    infoId:'',
-                    waitingAuditInfoId:'',
-                    realName:'',
-                    identity:'',
-                    sex : '',
-                    infoId:"",
-                    nation:'',
-                    academic:'',
-                    birthDate: ''
-                },
-                nameRules : function (value) {
-                    return {
-                      valid: value.length > 0,
-                      msg: '请输入正确的姓名'
-                    }
-                },
-                identityRules: function (value) {
-                    var reg = /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$)/;
-                    var result =  reg.test(value);
-                    return {
-                      valid: result,
-                      msg: '请输入正确的身份证号'
-                    }
-                }
-            }
-        },
-
-        mounted :function(){
-            var _this = this;
-            this.$nextTick(function () {
-                _this.getCertificationInfo();
-            });
-        },
-
-        methods : {
-
-            submitInfo : function () {
-
-                if (!this.userModel.infoId) {
-                    this.$vux.toast.text('请上传身份证照片', 'top')
-                    return;
-                }
-                if (!this.nameRules(this.userModel.realName).valid){
-                    this.$vux.toast.text('请输入正确的姓名', 'top')
-                    return;
-                }
-                if (!this.identityRules(this.userModel.identity).valid){
-                    this.$vux.toast.text('请输入正确的身份证号', 'top')
-                    return;
-                }
-
-                this.$vux.loading.show({
-                    text: '正在提交数据'
-                })
-
-                var _this = this;
-                _this.$post(_this.host_bdd + '/agent/v1/info/submit', {
-                    identity:_this.userModel.identity,
-                    name:_this.userModel.realName,
-                    gender:_this.userModel.sex,
-                    nation:_this.userModel.nation,
-                    birth:_this.userModel.birthDate,
-                    education:_this.userModel.academic,
-                    infoId:_this.userModel.infoId
-                }, function (rt) {
-                    _this.$vux.loading.hide();
-                    if (rt.data.code != 200) {
-                        _this.$vux.toast.text(rt.data.error, 'top')
-                        return;
-                    }
-                    _this.$vux.toast.text("提交成功", 'top')
-                    _this.$router.go(-1);
-                });
-            },
-
-            getCertificationInfo: function () {
-                var _this = this;
-                _this.$http.get(_this.host_bdd + '/agent/v1/info').then((rt) => {
-                    if (rt.data.code != 200) {
-                         _this.$vux.toast.text(rt.error, 'top');
-                         return;
-                     }
-                     if (rt.data.tenantAgentInfo) {
-
-                        _this.userModel.realName = rt.data.tenantAgentInfo.name;
-                        _this.userModel.identity = rt.data.tenantAgentInfo.identity;
-                        _this.userModel.sex = rt.data.tenantAgentInfo.gender;
-                        _this.userModel.nation = rt.data.tenantAgentInfo.nation;
-                        _this.userModel.academic = rt.data.tenantAgentInfo.education;
-                        _this.userModel.infoId = rt.data.tenantAgentInfo.id;
-                        _this.userModel.waitingAuditInfoId = rt.data.tenantAgentInfo.id;
-                        _this.userModel.birthDate = rt.data.tenantAgentInfo.birth;
-
-                        _this.canEdit = rt.data.tenantAgentInfo.state != 1;
-
-                        _this.$getImageUrlFromOSS(rt.data.tenantAgentInfo.frontPath,function (rt){
-                            if (rt){
-                                _this.imageSrcFront = rt;
-                            }
-                        });
-                        _this.$getImageUrlFromOSS(rt.data.tenantAgentInfo.backPath,function (rt){
-                            if (rt){
-                                _this.imageSrcBack = rt;
-                            }
-                        });
-                     }
-                 });
-            }
+  data () {
+    return {
+      canEdit: false,
+      imageSrcFront: '',
+      imageSrcBack: '',
+      userModel: {
+        infoId: '',
+        waitingAuditInfoId: '',
+        realName: '',
+        identity: '',
+        sex: '',
+        nation: '',
+        academic: '',
+        birthDate: ''
+      },
+      nameRules: function (value) {
+        return {
+          valid: value.length > 0,
+          msg: '请输入正确的姓名'
         }
+      },
+      identityRules: function (value) {
+        var reg = /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$)/
+        var result = reg.test(value)
+        return {
+          valid: result,
+          msg: '请输入正确的身份证号'
+        }
+      }
     }
+  },
+
+  mounted: function () {
+    var _this = this
+    this.$nextTick(function () {
+      _this.getCertificationInfo()
+    })
+  },
+
+  methods: {
+
+    submitInfo: function () {
+      if (!this.userModel.infoId) {
+        this.$vux.toast.text('请上传身份证照片', 'top')
+        return
+      }
+      if (!this.nameRules(this.userModel.realName).valid) {
+        this.$vux.toast.text('请输入正确的姓名', 'top')
+        return
+      }
+      if (!this.identityRules(this.userModel.identity).valid) {
+        this.$vux.toast.text('请输入正确的身份证号', 'top')
+        return
+      }
+
+      this.$vux.loading.show({
+        text: '正在提交数据'
+      })
+
+      var _this = this
+      _this.$post(_this.host_bdd + '/agent/v1/info/submit', {
+        identity: _this.userModel.identity,
+        name: _this.userModel.realName,
+        gender: _this.userModel.sex,
+        nation: _this.userModel.nation,
+        birth: _this.userModel.birthDate,
+        education: _this.userModel.academic,
+        infoId: _this.userModel.infoId
+      }, function (rt) {
+        _this.$vux.loading.hide()
+        if (rt.data.code !== 200) {
+          _this.$vux.toast.text(rt.data.error, 'top')
+          return
+        }
+        _this.$vux.toast.text('提交成功', 'top')
+        _this.$router.go(-1)
+      })
+    },
+
+    getCertificationInfo: function () {
+      var _this = this
+      _this.$http.get(_this.host_bdd + '/agent/v1/info').then((rt) => {
+        if (rt.data.code !== 200) {
+          _this.$vux.toast.text(rt.error, 'top')
+          return
+        }
+        if (rt.data.tenantAgentInfo) {
+          _this.userModel.realName = rt.data.tenantAgentInfo.name
+          _this.userModel.identity = rt.data.tenantAgentInfo.identity
+          _this.userModel.sex = rt.data.tenantAgentInfo.gender
+          _this.userModel.nation = rt.data.tenantAgentInfo.nation
+          _this.userModel.academic = rt.data.tenantAgentInfo.education
+          _this.userModel.infoId = rt.data.tenantAgentInfo.id
+          _this.userModel.waitingAuditInfoId = rt.data.tenantAgentInfo.id
+          _this.userModel.birthDate = rt.data.tenantAgentInfo.birth
+
+          _this.canEdit = rt.data.tenantAgentInfo.state !== 1
+
+          _this.$getImageUrlFromOSS(rt.data.tenantAgentInfo.frontPath, function (rt) {
+            if (rt) {
+              _this.imageSrcFront = rt
+            }
+          })
+          _this.$getImageUrlFromOSS(rt.data.tenantAgentInfo.backPath, function (rt) {
+            if (rt) {
+              _this.imageSrcBack = rt
+            }
+          })
+        }
+      })
+    }
+  }
+}
 </script>
