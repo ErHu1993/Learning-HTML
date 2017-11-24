13c7c80660e5d56140b9725c342a6ed2d75c93cc
胡广宇 <199301055@qq.com>
2017-11-24 14:48:13 +0800

    no message

--- 自己写的Vue(Vux)文件/component-views/income-chart.vue
+++ 自己写的Vue(Vux)文件/component-views/income-chart.vue
@@ -44,66 +44,65 @@
 </template>
 
 <script>
+import echarts from 'echarts'
+import $ from 'zepto'
 
-import echarts from 'echarts';
-import $ from 'zepto';
+export default {
 
-    export default {
-
-        data () {
-            return {
-                alreadyShow : false,
-                premiumChart : null
-            }
-        },
-
-        beforeDestroy () {
-            if (this.premiumChart.dispose) {
-                this.premiumChart.dispose();
-            }
-        },
-
-        components: {
-
-        },
-
-        props: {
-            chartId : null,
-            chartOption : null,
-            chartWidth : null,
-            chartTitle : null
-        },
-
-        watch:{
-            chartOption : {
-                handler: function (item) {
-                    if (item && this.chartWidth && !this.alreadyShow){
-                        this.alreadyShow = true;
-                        this.setChartOption(item,this.chartWidth);
-                    }
-                }
-            },
-            chartWidth : {
-                handler: function (item){
-                    if (this.chartOption && item && !this.alreadyShow){
-                        this.alreadyShow = true;
-                        this.setChartOption(this.chartOption,item);
-                    }
-                }
-            }
-        },
-
-        methods : {
-            //设置图标数据 , 标签id
-            setChartOption (chartOption,width) {
-                var chartDiv = document.getElementById(this.chartId);
-                chartDiv.style.width = width;
-                $('#' + this.chartId).parent().scrollLeft((parseInt(chartDiv.style.width) - parseInt(document.body.clientWidth)));
-                //初始化组件并绘制
-                this.premiumChart = echarts.init(chartDiv);
-                // 使用刚指定的配置项和数据显示图表。
-                this.premiumChart.setOption(chartOption);
-            }
-        }
+  data () {
+    return {
+      alreadyShow: false,
+      premiumChart: null
     }
+  },
+
+  beforeDestroy () {
+    if (this.premiumChart.dispose) {
+      this.premiumChart.dispose()
+    }
+  },
+
+  components: {
+
+  },
+
+  props: {
+    chartId: null,
+    chartOption: null,
+    chartWidth: null,
+    chartTitle: null
+  },
+
+  watch: {
+    chartOption: {
+      handler: function (item) {
+        if (item && this.chartWidth && !this.alreadyShow) {
+          this.alreadyShow = true
+          this.setChartOption(item, this.chartWidth)
+        }
+      }
+    },
+    chartWidth: {
+      handler: function (item) {
+        if (this.chartOption && item && !this.alreadyShow) {
+          this.alreadyShow = true
+          this.setChartOption(this.chartOption, item)
+        }
+      }
+    }
+  },
+
+  methods: {
+            // 设置图标数据 , 标签id
+    setChartOption (chartOption, width) {
+      var chartDiv = document.getElementById(this.chartId)
+      chartDiv.style.width = width
+      $('#' + this.chartId).parent().scrollLeft((parseInt(chartDiv.style.width) - parseInt(document.body.clientWidth)))
+                // 初始化组件并绘制
+      this.premiumChart = echarts.init(chartDiv)
+                // 使用刚指定的配置项和数据显示图表。
+      this.premiumChart.setOption(chartOption)
+    }
+  }
+}
 </script>
