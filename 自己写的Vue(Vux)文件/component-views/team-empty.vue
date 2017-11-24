2927386ed1ca991f4bfce62400c2761c17c36c84
胡广宇 <199301055@qq.com>
2017-11-24 14:48:23 +0800

    no message

--- 自己写的Vue(Vux)文件/component-views/team-empty.vue
+++ 自己写的Vue(Vux)文件/component-views/team-empty.vue
@@ -30,27 +30,26 @@
 <script>
     export default {
 
-        data () {
-            return {
-                imageSrc : "",
-            }
-        },
-
-        mounted :function(){
-            this.$nextTick(function () {
-                var title = this.$route.query.title;;
-                if (title == "津贴明细") {
-                    this.imageSrc = require("../../assets/alowance-empty.png")
-                }else {
-                    this.imageSrc = require("../../assets/team-list-empty.png")
-                }
-                document.title = title;
-            });
-        },
-        methods : {
-            goInvite () {
-                this.$router.push({ path: '/invite' });
-            }
+      data () {
+        return {
+          imageSrc: ''
         }
+      },
+      mounted: function () {
+        this.$nextTick(function () {
+          var title = this.$route.query.title
+          if (title === '津贴明细') {
+            this.imageSrc = require('../../assets/alowance-empty.png')
+          } else {
+            this.imageSrc = require('../../assets/team-list-empty.png')
+          }
+          document.title = title
+        })
+      },
+      methods: {
+        goInvite () {
+          this.$router.push({ path: '/invite' })
+        }
+      }
     }
 </script>
