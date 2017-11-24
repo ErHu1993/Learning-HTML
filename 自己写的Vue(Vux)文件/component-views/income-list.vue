559a8d61f1d567ce91b6f80a9863ae3afbc779da
胡广宇 <199301055@qq.com>
2017-11-24 14:48:21 +0800

    no message

--- 自己写的Vue(Vux)文件/component-views/income-list.vue
+++ 自己写的Vue(Vux)文件/component-views/income-list.vue
@@ -69,209 +69,209 @@
 </template>
 
 <script>
+import $ from 'zepto'
+import { formatDate } from '@/libs/format.js'
+import { Scroller, LoadMore } from 'vux'
 
-import $ from 'zepto';
-import { formatDate } from '@/libs/format.js';
-import { Scroller,LoadMore } from 'vux'
+export default {
 
-    export default {
+  components: {
+    Scroller, LoadMore
+  },
 
-        components: {
-            Scroller, LoadMore
-        },
+  data () {
+    return {
+      onFetching: false,
+      bottomCount: 20,
+      pageNo: 0,
+      listHeight: 0,
+      status: {
+        pullupStatus: 'default'
+      },
+      inoutcomes: [],
+      pageNumber: 0,
+      pageCount: 0,
+      pageSize: 20,
+      emptyData: false
+    }
+  },
 
-        data () {
-            return {
-                onFetching: false,
-                bottomCount: 20,
-                pageNo: 0,
-                listHeight: 0,
-                status: {
-                    pullupStatus: 'default'
-                },
-                inoutcomes: [],
-                pageNumber: 0,
-                pageCount: 0,
-                pageSize: 20,
-                emptyData : false
-            }
-        },
+  watch: {
+    dateParam: {
+      handler: function (item) {
+        this.initData()
+        this.getInoutCome()
+      }
+    }
+  },
 
-        watch:{
-            dateParam : {
-                handler: function (item) {
-                    this.initData();
-                    this.getInoutCome();
-                }
-            }
-        },
-
-        props: {
+  props: {
             /**
              * 1:佣金列表  2:津贴列表  3:保费列表
              */
-            category : 0,
-            dateParam : "",
-            marginTop: 0,
-            noData: false
-        },
+    category: 0,
+    dateParam: '',
+    marginTop: 0,
+    noData: false
+  },
 
-        filters : {
+  filters: {
             // 时间格式化
-            formatData (time) {
-                let date = new Date(time * 1000);
-                return formatDate(date, 'yyyy-MM-dd hh:mm:ss');
-            }
-        },
+    formatData (time) {
+      let date = new Date(time * 1000)
+      return formatDate(date, 'yyyy-MM-dd hh:mm:ss')
+    }
+  },
 
-        created () {
-            this.listHeight = $(window).height() - parseInt(this.marginTop);
-        },
+  created () {
+    this.listHeight = $(window).height() - parseInt(this.marginTop)
+  },
 
-        mounted () {
-            this.$nextTick(() => {
-                if (this.inoutcomes.length){
-                    this.$refs.scrollerBottom.reset({top:0});
-                }
-                this.getInoutCome();
-            });
-        },
+  mounted () {
+    this.$nextTick(() => {
+      if (this.inoutcomes.length) {
+        this.$refs.scrollerBottom.reset({top: 0})
+      }
+      this.getInoutCome()
+    })
+  },
 
-        updated() {
-            this.$nextTick(() => {
-                this.$refs.scrollerBottom.donePulldown();
-                this.$refs.scrollerBottom.reset();
+  updated () {
+    this.$nextTick(() => {
+      this.$refs.scrollerBottom.donePulldown()
+      this.$refs.scrollerBottom.reset()
 
-                const boxH = $('.box2').height(), listH = $('.box2').closest('.list').height();
-                if (boxH <= listH) {
-                    $('.weui-loadmore__tips')
+      const boxH = $('.box2').height()
+      const listH = $('.box2').closest('.list').height()
+      if (boxH <= listH) {
+        $('.weui-loadmore__tips')
                     .text('数据全部加载完成！')
                     .prev().hide()
-                    .parent().css('margin-top', listH - boxH);
-                }
-            });
-        },
+                    .parent().css('margin-top', listH - boxH)
+      }
+    })
+  },
 
-        methods : {
-            getInoutCome () {
-                switch (parseInt(this.category)) {
-                    case 1 : {
-                        this.getCommissionDataList({
-                            month: this.dateParam,
-                            pn: this.pageNumber + 1,
-                            ps: this.pageSize
-                        })
-                        break;
-                    }
-                    case 2 : {
-                        this.getAllowanceDataList();
-                        break;
-                    }
-                    case 3 : {
-                        this.getPremiumDataList({
-                            month: this.dateParam,
-                            pn: this.pageNumber + 1,
-                            ps: this.pageSize
-                        })
-                        break;
-                    }
-                }
-            },
-            // 到底加载
-            onScrollBottom () {
-                if (this.onFetching) return;
-                if ((this.pageCount == this.pageNumber))  {
-                    $('.weui-loadmore__tips').text('数据全部加载完成！').prev().hide();
-                    return;
-                }
-                this.onFetching = true;
-                this.getInoutCome();
-            },
-            // 下拉刷新
-            onPulldown () {
-                this.initData();
-                this.getInoutCome();
-            },
-            // 初始化
-            initData () {
-                this.pageNumber = 0;
-                this.pageCount = 0;
-                this.inoutcomes = [];
-                this.onFetching = true;
-                $('.weui-loadmore__tips').text('loading').prev().show();
-            },
-
-            getCommissionDataList (params) {
-                var _this = this;
-                _this.$http.get(_this.host_bdd + '/captial/v1/commission/list', {
-                    params: params
-                }).then((rt) => {
-                    if (rt.data.code != 200) {
-                        this.$vux.toast.text(rt.data.error, 'top');
-                        return;
-                    }
-                    // 多页数据进行合并
-                    if (rt.data.inoutcomes) {
-                        _this.inoutcomes = _this.inoutcomes.concat(rt.data.inoutcomes);
-                        _this.pageNumber = rt.data.pageNumber;
-                        _this.pageCount = rt.data.pageCount;
-                    }
-
-                    _this.emptyData = _this.inoutcomes.length == 0;
-
-                    setTimeout( () => {
-                        _this.onFetching = false;
-                    }, 600);
-                });
-            },
-
-            getAllowanceDataList () {
-                var _this = this;
-                _this.$http.get(_this.host_bdd + '/captial/v1/allowance/list?year=' + _this.dateParam)
-                .then((rt) => {
-                    if (rt.data.code != 200) {
-                         _this.$vux.toast.text(rt.error, 'top');
-                         return;
-                     }
-                    // 多页数据进行合并
-                     if (rt.data.inoutcomes) {
-                        _this.inoutcomes = _this.inoutcomes.concat(rt.data.inoutcomes);
-                     }
-
-                     _this.emptyData = _this.inoutcomes.length == 0;
-
-                     _this.pageCount = 1;
-                     _this.pageNumber = 1;
-
-                    setTimeout( () => {
-                        _this.onFetching = false;
-                    }, 600);
-                 });
-            },
-
-            getPremiumDataList (params) {
-                var _this = this;
-                _this.$http.get(_this.host_bdd + '/captial/v1/premium/list', {
-                    params: params
-                }).then((rt) => {
-                    if (rt.data.code != 200) {
-                        this.$vux.toast.text(rt.data.error, 'top');
-                        return;
-                    }
-                    // 多页数据进行合并
-                    if (rt.data.inoutcomes) {
-                        _this.inoutcomes = _this.inoutcomes.concat(rt.data.inoutcomes);
-                        _this.pageNumber = rt.data.pageNumber;
-                        _this.pageCount = rt.data.pageCount;
-                    }
-
-                    _this.emptyData = _this.inoutcomes.length == 0;
-
-                    setTimeout( () => {
-                        _this.onFetching = false;
-                    }, 600);
-                });
-            }
+  methods: {
+    getInoutCome () {
+      switch (parseInt(this.category)) {
+        case 1 : {
+          this.getCommissionDataList({
+            month: this.dateParam,
+            pn: this.pageNumber + 1,
+            ps: this.pageSize
+          })
+          break
         }
+        case 2 : {
+          this.getAllowanceDataList()
+          break
+        }
+        case 3 : {
+          this.getPremiumDataList({
+            month: this.dateParam,
+            pn: this.pageNumber + 1,
+            ps: this.pageSize
+          })
+          break
+        }
+      }
+    },
+            // 到底加载
+    onScrollBottom () {
+      if (this.onFetching) return
+      if ((this.pageCount === this.pageNumber)) {
+        $('.weui-loadmore__tips').text('数据全部加载完成！').prev().hide()
+        return
+      }
+      this.onFetching = true
+      this.getInoutCome()
+    },
+            // 下拉刷新
+    onPulldown () {
+      this.initData()
+      this.getInoutCome()
+    },
+            // 初始化
+    initData () {
+      this.pageNumber = 0
+      this.pageCount = 0
+      this.inoutcomes = []
+      this.onFetching = true
+      $('.weui-loadmore__tips').text('loading').prev().show()
+    },
+
+    getCommissionDataList (params) {
+      var _this = this
+      _this.$http.get(_this.host_bdd + '/captial/v1/commission/list', {
+        params: params
+      }).then((rt) => {
+        if (rt.data.code !== 200) {
+          this.$vux.toast.text(rt.data.error, 'top')
+          return
+        }
+                    // 多页数据进行合并
+        if (rt.data.inoutcomes) {
+          _this.inoutcomes = _this.inoutcomes.concat(rt.data.inoutcomes)
+          _this.pageNumber = rt.data.pageNumber
+          _this.pageCount = rt.data.pageCount
+        }
+
+        _this.emptyData = _this.inoutcomes.length === 0
+
+        setTimeout(() => {
+          _this.onFetching = false
+        }, 600)
+      })
+    },
+
+    getAllowanceDataList () {
+      var _this = this
+      _this.$http.get(_this.host_bdd + '/captial/v1/allowance/list?year=' + _this.dateParam)
+                .then((rt) => {
+                  if (rt.data.code !== 200) {
+                    _this.$vux.toast.text(rt.error, 'top')
+                    return
+                  }
+                    // 多页数据进行合并
+                  if (rt.data.inoutcomes) {
+                    _this.inoutcomes = _this.inoutcomes.concat(rt.data.inoutcomes)
+                  }
+
+                  _this.emptyData = _this.inoutcomes.length === 0
+
+                  _this.pageCount = 1
+                  _this.pageNumber = 1
+
+                  setTimeout(() => {
+                    _this.onFetching = false
+                  }, 600)
+                })
+    },
+
+    getPremiumDataList (params) {
+      var _this = this
+      _this.$http.get(_this.host_bdd + '/captial/v1/premium/list', {
+        params: params
+      }).then((rt) => {
+        if (rt.data.code !== 200) {
+          this.$vux.toast.text(rt.data.error, 'top')
+          return
+        }
+                    // 多页数据进行合并
+        if (rt.data.inoutcomes) {
+          _this.inoutcomes = _this.inoutcomes.concat(rt.data.inoutcomes)
+          _this.pageNumber = rt.data.pageNumber
+          _this.pageCount = rt.data.pageCount
+        }
+
+        _this.emptyData = _this.inoutcomes.length === 0
+
+        setTimeout(() => {
+          _this.onFetching = false
+        }, 600)
+      })
     }
+  }
+}
 </script>
