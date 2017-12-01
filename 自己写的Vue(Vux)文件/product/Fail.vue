5cd77b2cc71d709aff1caef02ae578dafed6a2a3
胡广宇 <199301055@qq.com>
2017-12-01 16:38:19 +0800

    no message

--- 自己写的Vue(Vux)文件/product/Fail.vue
+++ 自己写的Vue(Vux)文件/product/Fail.vue
@@ -17,35 +17,28 @@
 </template>
 
 <script>
-import $ from 'zepto';
-import { Msg, Divider, XButton } from 'vux';
+import { Msg, Divider, XButton } from 'vux'
 export default {
-    data () {
-        return {
-            description: '支付金额$600.00'
-        }
-    },
-    components: {
-        Msg, Divider, XButton
-    },
-    computed: {
-
-    },
-    created () {
-        var _this = this;
-
-    },
-    methods: {
-        fmoney(s, n) {
-            n = n > 0 && n <= 20 ? n : 2;
-            s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
-            var l = s.split(".")[0].split("").reverse(), r = s.split(".")[1];
-            var t = "";
-            for (var i = 0; i < l.length; i++) {
-                t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
-            }
-            return t.split("").reverse().join("") + "." + r;
-        }
+  data () {
+    return {
+      description: '支付金额$600.00'
     }
+  },
+  components: {
+    Msg, Divider, XButton
+  },
+  methods: {
+    fmoney (s, n) {
+      n = n > 0 && n <= 20 ? n : 2
+      s = parseFloat((s + '').replace(/[^\d.-]/g, '')).toFixed(n) + ''
+      var l = s.split('.')[0].split('').reverse()
+      var r = s.split('.')[1]
+      var t = ''
+      for (var i = 0; i < l.length; i++) {
+        t += l[i] + ((i + 1) % 3 === 0 && (i + 1) !== l.length ? ',' : '')
+      }
+      return t.split('').reverse().join('') + '.' + r
+    }
+  }
 }
 </script>
